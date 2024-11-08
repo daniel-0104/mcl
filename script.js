f07cdd7a16d25f7124cd7e3e0d8299010ff7e74b
Daniel <122777459+daniel-0104@users.noreply.github.com>
2024-11-08 23:58:07 +0630

    new commit

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 // href clicked active link end
+
+
+//................................. category click start ...........................
+let detailsText = document.getElementsByClassName('text-content');
+const monthPlan = document.getElementById('monthly-plan');
+const yearPlan = document.getElementById('yearly-plan');
+const ownerPlan = document.getElementById('owner-plan');
+let currentDetails = 0;
+
+function detailsClick(event,elementId){
+  event.preventDefault();
+  const clickText = event.target;
+
+  if(clickText.classList.contains('text-active')){
+    return;
+  }
+  for(let d of detailsText){
+    d.classList.remove('text-active');
+  }
+  clickText.classList.add('text-active');
+  currentDetails = Array.from(detailsText).indexOf(clickText);
+
+  monthPlan.style.display = elementId === 'monthPlan' ? 'block' : 'none';
+  yearPlan.style.display = elementId === 'yearPlan' ? 'block' : 'none';
+  ownerPlan.style.display = elementId === 'ownerPlan' ? 'block' : 'none';
+}
+//........................................ category click end ................................
